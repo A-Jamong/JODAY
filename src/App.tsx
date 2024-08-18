ce94ea4b9f1470e18a1c558bdb851326801c3d13
JAM <99.1.some@gmail.com>
2024-08-18 20:27:02 +0900

    [수정] 폴더링 후 경로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 
-import Logo from "./components/logo";
-import Title from "./components/Title";
-import TodayDate from "./components/Date";
-import Button from "./components/Button";
-import Navigation from "./components/Navigation";
+import Logo from "./components/Logo/Logo";
+import Title from "./components/Title/Title";
+import TodayDate from "./components/Date/Date";
+import Button from "./components/Button/Button";
+import Navigation from "./components/Navigation/Navigation";
 // import { TodoList } from "./components/TodoList";
 
 const App = () => {
   return (
     <div>
       <Logo type="stereo" />
-      <Title titleText="우리, 오늘 뭐할까?" />
+      <Title titleText="우리, 오늘 뭐할까?!" />
       <TodayDate />
       <Button label="생각났어?" type="primary" isShowIcon />
       <Navigation
