84ed38f1532cdbba36f034289673ea4b93ca0641
JAM <99.1.some@gmail.com>
2024-08-18 18:58:18 +0900

    [수정] 배포 오류 해결중

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,27 @@
-import { defineConfig } from 'vite';
-import { fileURLToPath, URL} from 'node:url';
-import pluginReact from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "node:url";
+import pluginReact from "@vitejs/plugin-react";
 
 const viteConfig = defineConfig({
-  base:'/',
-  server:{
-    host: 'localhost',
-    port:3000,
+  base: "/",
+  server: {
+    host: "localhost",
+    port: 3000,
   },
   plugins: [
-    pluginReact(
-     { jsxRuntime :'automatic'}
-    ),
+    pluginReact({
+      jsxRuntime: "automatic",
+    }),
   ],
-  resolve:{
-    alias:{
+  resolve: {
+    alias: {
       // 사용자가 @을 쓰게 되면 그것은 src 폴더를 가리키게 하겠다.
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  build: {
+    outDir: "dist", // Netlify 배포 디렉터리
+  },
 });
 
 export default viteConfig;
